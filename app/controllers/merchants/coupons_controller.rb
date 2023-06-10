d39623252d4d44514962ac0729e74281922d36cb
Jesse Thomas <jgthomas3@gmail.com>
2023-06-10 17:37:46 -0600

    Feat: Create and new methods

--- app/controllers/merchants/coupons_controller.rb
+++ app/controllers/merchants/coupons_controller.rb
@@ -4,6 +4,31 @@ class Merchants::CouponsController < ApplicationController
   end
 
   def show
-    
+
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @coupon = Coupon.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    coupon = @merchant.coupons.new(coupon_params)
+    if @merchant.active_coupons_at_max?(5)
+      redirect_to new_merchant_coupon_path(@merchant)
+      flash[:alert] = "Maximum coupons created, please deactivate one to create a new coupon"
+    elsif coupon.save
+      redirect_to merchant_coupons_path(@merchant)
+    else
+      redirect_to new_merchant_coupon_path(@merchant)
+      flash[:alert] = "Please fill out all fields"
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :code, :status, :discount_type, :discount_amount)
   end
 end
