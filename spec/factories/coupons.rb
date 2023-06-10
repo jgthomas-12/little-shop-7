914d4d0976d416f6667cf42c65bb792ab9641b3a
Jesse Thomas <jgthomas3@gmail.com>
2023-06-10 10:06:53 -0600

    Refact: adjust coupon factory

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     code { Faker::Alphanumeric.alphanumeric(number: 5) }
     status { rand(0..1) }
     discount_type { rand(0..1) }
-    discount_amount { rand(10..50) }
+    discount_amount { rand(0..50) }
+    merchant_id { rand(1..20) }
   end
 end
