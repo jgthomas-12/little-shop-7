53a240a0568edff5776bcb9f816b10e94715f583
Jesse Thomas <jgthomas3@gmail.com>
2023-06-11 09:32:12 -0600

    Feat: add update method to controller

--- app/controllers/merchants/coupons_controller.rb
+++ app/controllers/merchants/coupons_controller.rb
@@ -4,6 +4,7 @@ class Merchants::CouponsController < ApplicationController
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
     @coupon = Coupon.find(params[:id])
   end
 
@@ -23,6 +24,15 @@ class Merchants::CouponsController < ApplicationController
     else
       redirect_to new_merchant_coupon_path(@merchant)
       flash[:alert] = "Please fill out all fields and make sure code is unique"
+    end
+  end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    coupon = Coupon.find(params[:id])
+    if params[:status] == "0"
+      coupon.update(status: 0)
+      redirect_to merchant_coupon_path(merchant, coupon)
     end
   end
 
